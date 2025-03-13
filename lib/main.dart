9718e99e09be487e9bd7c4232c4e094220c12cf1
Manatchai <65143038@g.cmru.ac.th>
2025-03-13 10:37:58 +0700

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:project2_1/services/storage_service.dart';
 import 'package:project2_1/views/login_view.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await StorageServixe().init();
+  await StorageService().init();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await StorageService().init();
   runApp(const MyApp());
-}
-
-class StorageServixe {
-  init() {}
 }
 
 class MyApp extends StatelessWidget {
